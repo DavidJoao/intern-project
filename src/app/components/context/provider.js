726f64efc19837c05d16046139b160c6135a8c4c
DavidJoao <davidsandoval596@gmail.com>
2024-12-02 19:20:43 -0800

    ADDED I18Next Provider to Context Provider

--- src/app/components/context/provider.js
+++ src/app/components/context/provider.js
@@ -1,21 +1,25 @@
 "use client"
- import { SessionProvider } from 'next-auth/react'
- import { createContext, useContext, useState } from 'react'
+import { SessionProvider } from 'next-auth/react'
+import { createContext, useContext, useState } from 'react'
+import { I18nextProvider } from 'react-i18next';
+import i18next from '../../lib/i18n';
 
- const AppContext = createContext();
+const AppContext = createContext();
 
- const Provider = ( { children } ) => {
+const Provider = ( { children } ) => {
 
     const [appNotification, setAppNotification] = useState("")
 
     return (
         <SessionProvider>
-            <AppContext.Provider value={{ appNotification, setAppNotification }}>
+            <I18nextProvider i18n={i18next}>
+                <AppContext.Provider value={{ appNotification, setAppNotification }}>
                 {children}
-            </AppContext.Provider>
+                </AppContext.Provider>
+            </I18nextProvider>
         </SessionProvider>
     )
  }
  export default Provider
 
- export const useAppContext = () => useContext(AppContext);
+ export const useAppContext = () => useContext(AppContext); 
