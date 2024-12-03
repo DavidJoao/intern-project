07cd0786b23a1fd1a928c093744e07eee80e6825
DavidJoao <davidsandoval596@gmail.com>
2024-12-02 19:21:36 -0800

    CREATED: Example of functionality for changing languages

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
-export default function Home() {
-  return (
-    <div>
+'use client'
+import { useTranslation } from "react-i18next";
 
-    </div>
-  );
+export default function Home() {
+
+    const { t, i18n } = useTranslation('common');
+
+    const changeLanguage = async (lng) => {
+        await i18n.changeLanguage(lng);
+    }
+
+    return (
+        <div>
+            <h1>{t('welcome')}</h1>
+            <p>{t('description')}</p>
+            <button onClick={() => changeLanguage('en')}>English</button>
+            <button onClick={() => changeLanguage('es')}>Spanish</button>
+        </div>
+    );
 }
