f46fca6bad584fdcb27c931ac5c625acd41ac1d7
DavidJoao <davidsandoval596@gmail.com>
2024-12-02 19:54:18 -0800

    EDIT: Commented Out code that will be useful for navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 'use client'
 import { useTranslation } from "react-i18next";
+import { useAppContext } from "./components/context/provider";
 
 export default function Home() {
 
+    const { toggleTheme } = useAppContext()
     const { t, i18n } = useTranslation('common');
 
     const changeLanguage = async (lng) => {
@@ -11,10 +13,11 @@ export default function Home() {
 
     return (
         <div>
-            <h1>{t('welcome')}</h1>
+            {/* <h1>{t('welcome')}</h1>
             <p>{t('description')}</p>
             <button onClick={() => changeLanguage('en')}>English</button>
             <button onClick={() => changeLanguage('es')}>Spanish</button>
+            <button onClick={() => toggleTheme()}>Toggle</button> */}
         </div>
     );
 }
