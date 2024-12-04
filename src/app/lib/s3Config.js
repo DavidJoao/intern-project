0dc1618581f34b677de6b5a7dd4a7a48d72099fb
DavidJoao <davidsandoval596@gmail.com>
2024-12-03 23:43:12 -0800

    INITIALIZED: Config for S3 Bucket

---
+++ src/app/lib/s3Config.js
@@ -0,0 +1,12 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: process.env(AWS_REGION),
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+})
+
+const s3 = new AWS.s3();
+module.exports = s3;
