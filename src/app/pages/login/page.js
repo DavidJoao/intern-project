07a39c3c69e80684323e9fb67b916e2ccc7cea5c
DavidJoao <davidsandoval596@gmail.com>
2024-12-03 23:42:16 -0800

    CREATED: First version of login page

--- src/app/pages/login/page.js
+++ src/app/pages/login/page.js
@@ -1,9 +1,29 @@
+'use client'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const Login = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = async (data) => {
+        
+    }
+
+    return (
+        <div className='w-screen h-screen center-col basic-theme'>
+            <div className='w-auto h-auto border-[1px] border-black center-col p-3 gap-3'>
+                <h1 className='font-bold text-3xl'>Login</h1>
+                <form className='border-[1px] border-primary dark:border-none rounded w-[250px] md:w-[400px] lg:w-[500px] p-3 white-theme center-col gap-3' onSubmit={handleSubmit(onSubmit)}>
+                    <label>Email</label>
+                    <input required className='input w-[70%]' {...register("email", { required: true })}/>
+                    <label>Password</label>
+                    <input required className='input w-[70%]' type='password'  {...register("password", { required: true })}/>
+                    <button type='submit' className='theme-button w-[70%]'>Login</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
-export default page
+export default Login
