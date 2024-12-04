2c7a3edfda6ad6b727bc02088de5c6f7878a24a4
DavidJoao <davidsandoval596@gmail.com>
2024-12-03 23:41:12 -0800

    CHANGE: Set Theme useEffect to tailwind

--- src/app/components/context/provider.js
+++ src/app/components/context/provider.js
@@ -12,24 +12,24 @@ const Provider = ( { children } ) => {
     const [theme, setTheme] = useState('light');
 
     useEffect(() => {
-        const savedTheme = localStorage.getItem('theme');
-        if (savedTheme) {
-          document.documentElement.setAttribute('data-theme', savedTheme);
-          setTheme(savedTheme);
-        } else {
-          const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-          const initialTheme = prefersDarkScheme ? 'dark' : 'light';
-          document.documentElement.setAttribute('data-theme', initialTheme);
-          setTheme(initialTheme);
-        }
-      }, []);
-
-      const toggleTheme = () => {
-        const newTheme = theme === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
-        setTheme(newTheme);
-        localStorage.setItem('theme', newTheme);
-      };
+      const savedTheme = localStorage.getItem('theme');
+      if (savedTheme) {
+        document.documentElement.classList.toggle('dark', savedTheme === 'dark');
+        setTheme(savedTheme);
+      } else {
+        const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const initialTheme = prefersDarkScheme ? 'dark' : 'light';
+        document.documentElement.classList.toggle('dark', initialTheme === 'dark');
+        setTheme(initialTheme);
+      }
+    }, []);
+  
+    const toggleTheme = () => {
+      const newTheme = theme === 'dark' ? 'light' : 'dark';
+      document.documentElement.classList.toggle('dark', newTheme === 'dark');
+      setTheme(newTheme);
+      localStorage.setItem('theme', newTheme);
+    };
 
     return (
         <SessionProvider>
