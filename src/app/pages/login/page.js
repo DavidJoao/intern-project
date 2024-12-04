1c51edd6d9f00e6651471ac95938f7c4124e77f0
DavidJoao <davidsandoval596@gmail.com>
2024-12-04 00:02:07 -0800

    EDIT: Styling in login and signup pages

--- src/app/pages/login/page.js
+++ src/app/pages/login/page.js
@@ -1,18 +1,20 @@
 'use client'
+import { navigate } from '@/app/lib/redirect';
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import Link from 'next/link';
 
 const Login = () => {
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
     const onSubmit = async (data) => {
-        
+
     }
 
     return (
         <div className='w-screen h-screen center-col basic-theme'>
-            <div className='w-auto h-auto border-[1px] border-black center-col p-3 gap-3'>
+            <div className='w-auto h-auto center-col p-3 gap-3'>
                 <h1 className='font-bold text-3xl'>Login</h1>
                 <form className='border-[1px] border-primary dark:border-none rounded w-[250px] md:w-[400px] lg:w-[500px] p-3 white-theme center-col gap-3' onSubmit={handleSubmit(onSubmit)}>
                     <label>Email</label>
@@ -21,6 +23,7 @@ const Login = () => {
                     <input required className='input w-[70%]' type='password'  {...register("password", { required: true })}/>
                     <button type='submit' className='theme-button w-[70%]'>Login</button>
                 </form>
+                <Link className='hover:underline underline-offset-2' href={'/pages/signup'}>Don't have an account? Signup!</Link>
             </div>
         </div>
     )
--- src/app/pages/signup/page.js
+++ src/app/pages/signup/page.js
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client'
+import React from "react"
+import { useForm } from "react-hook-form"
+import { navigate } from "@/app/lib/redirect"
+import Link from "next/link"
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const Signup = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = async (data) => {
+        console.log(data)
+    }
+
+	return (
+		<div className="w-screen h-screen center-col basic-theme">
+			<div className="w-auto h-auto center-col p-3 gap-3">
+				<h1 className="font-bold text-3xl">Login</h1>
+				<form className="border-[1px] border-primary dark:border-none rounded w-[250px] md:w-[400px] lg:w-[500px] p-3 white-theme center-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+					<label>Email</label>
+					<input required className="input w-[70%]" {...register("email", { required: true })}/>
+					<label>Password</label>
+					<input required className="input w-[70%]" type="password" {...register("password", { required: true })}/>
+					<button type="submit" className="theme-button w-[70%]"> Signup </button>
+				</form>
+                <Link className='hover:underline underline-offset-2' href={'/pages/login'}>Already have an account? Login!</Link>
+			</div>
+		</div>
+	)
 }
 
-export default page
+export default Signup
