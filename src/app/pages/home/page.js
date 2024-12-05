f3ab300939fd3bd1cfd96472ee49f2c7b418a3d7
DavidJoao <davidsandoval596@gmail.com>
2024-12-04 22:25:15 -0800

    EDIT: Created and commented out function to log current user

--- src/app/pages/home/page.js
+++ src/app/pages/home/page.js
@@ -1,8 +1,18 @@
-import React from 'react'
+'use client'
+import useAuth from '@/app/hooks/useAuth'
+import React, { useEffect } from 'react'
 
 const page = () => {
+  
+//   const logUser = async () => {
+//     const user = await useAuth();
+//     console.log(user)
+//    }
+
   return (
-    <div>page</div>
+    <div>
+      
+    </div>
   )
 }
 
