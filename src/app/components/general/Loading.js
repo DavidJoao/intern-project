1f22112d59c31afbdfdf421d745f8ec717cf6546
DavidJoao <davidsandoval596@gmail.com>
2024-12-09 23:42:05 -0800

    CREATED: Loading Screen component

---
+++ src/app/components/general/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen text-center'>
+        <p className='text-center text-2xl font-semibold'>Loading, please wait...</p>
+    </div>
+  )
+}
+
+export default Loading
