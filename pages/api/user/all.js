85e2549e4512328a7789a66e82d7980410ea27f3
DavidJoao <davidsandoval596@gmail.com>
2024-12-10 15:41:01 -0800

    CREATED: Server route to get all users

---
+++ pages/api/user/all.js
@@ -0,0 +1,16 @@
+import { db } from "@/app/lib/prismaClient";
+
+export default async function GET(req, res){
+    try {
+        const allUsersWithoutPassword = []
+        const allUsers = await db.user.findMany({})
+        allUsers.forEach((user, index) => {
+            const { password, ...userWithoutPassword } = user;
+            allUsersWithoutPassword.push(userWithoutPassword)
+        })
+        res.status(200).json({ users: allUsersWithoutPassword });
+    } catch (error) {
+        res.status(500).json({ error: error })
+        return error;
+    }
+}
