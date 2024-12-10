18edc8e0c0837123ac445653d793ea41fefa75ad
DavidJoao <davidsandoval596@gmail.com>
2024-12-09 23:42:19 -0800

    CREATED: Navbar component

---
+++ src/app/components/general/Navbar.js
@@ -0,0 +1,41 @@
+'use client'
+import React from 'react'
+import { logoutUser } from '@/app/actions/session'
+import Link from 'next/link'
+import { useTranslation } from "react-i18next";
+import { useAppContext } from '../context/provider';
+
+const Navbar = ({ session }) => {
+
+    const { toggleTheme } = useAppContext()
+    const { t, i18n } = useTranslation('common');
+
+    const changeLanguage = async (lng) => {
+        await i18n.changeLanguage(lng);
+    }
+
+    return (
+        <>
+            <div className='border-[1px] border-black w-screen h-[50px] flex flex-row-reverse items-center p-3 gap-3 fixed'>
+                {session ? (
+                    <>
+                    <button onClick={() => logoutUser()}>Logout</button>
+                    <Link href={'/pages/profile'}>Profle</Link>
+                    <Link href={'/pages/home'}>Dashboard</Link>
+                    <button onClick={() => changeLanguage('en')}>English</button>
+                    <button onClick={() => changeLanguage('es')}>Spanish</button>
+                    <button className="" onClick={() => toggleTheme()}>Toggle</button>
+                </>
+                ) : (
+                    <>
+                    <button onClick={() => changeLanguage('en')}>English</button>
+                    <button onClick={() => changeLanguage('es')}>Spanish</button>
+                    <button className="" onClick={() => toggleTheme()}>Toggle</button>
+                    </>
+            )}
+            </div>
+        </>
+  )
+}
+
+export default Navbar
