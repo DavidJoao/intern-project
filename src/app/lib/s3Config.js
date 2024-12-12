51b91840d7a33356aac1568b4d8170faaf60bd0f
DavidJoao <davidsandoval596@gmail.com>
2024-12-11 21:00:41 -0800

    FIX: Wrong initial configuration and initialization

--- src/app/lib/s3Config.js
+++ src/app/lib/s3Config.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
 
 AWS.config.update({
-    region: process.env(AWS_REGION),
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
     }
 })
 
-const s3 = new AWS.s3();
+const s3 = new AWS.S3();
 module.exports = s3;
