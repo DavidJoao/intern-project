7abfbf8948564f95c3f9bca5c9710587d225e285
DavidJoao <davidsandoval596@gmail.com>
2024-12-11 21:03:06 -0800

    PRISMA Migration

---
+++ prisma/migrations/20241212003530_new_template_schema/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - The values [single_line,checkbox] on the enum `QuestionType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "QuestionType_new" AS ENUM ('text', 'multi_line', 'integer');
+ALTER TABLE "Question" ALTER COLUMN "type" TYPE "QuestionType_new" USING ("type"::text::"QuestionType_new");
+ALTER TYPE "QuestionType" RENAME TO "QuestionType_old";
+ALTER TYPE "QuestionType_new" RENAME TO "QuestionType";
+DROP TYPE "QuestionType_old";
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "Template" ALTER COLUMN "isPublic" SET DEFAULT true;
