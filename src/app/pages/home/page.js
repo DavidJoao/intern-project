643722a2d1a099a2bfcdf3597bc04ec1abd8bfef
DavidJoao <davidsandoval596@gmail.com>
2024-12-12 18:50:34 -0800

    CHANGE: Separated ExploreTemplates to a component, deleted comments

--- src/app/pages/home/page.js
+++ src/app/pages/home/page.js
@@ -1,6 +1,7 @@
 'use client'
 import Loading from '@/app/components/general/Loading'
 import CreateTemplate from '@/app/components/templates/CreateTemplate'
+import ExploreTemplates from '@/app/components/templates/ExploreTemplates'
 import { useAuth } from '@/app/hooks/useAuth'
 import { icons } from '@/app/lib/icons'
 import React, { useEffect } from 'react'
@@ -14,27 +15,20 @@ const Home = () => {
   return (
     <>
     {user ? (
-        <div className='w-screen h-screen flex flex-col pt-[50px]'>
-			{/* Search Section */}
-			<div className='border-[1px] border-black h-[10%] flex items-center justify-center p-3'>
-				<form className='border-[1px] border-red-500 h-auto w-auto flex flex-row items-center justify-center gap-2 p-5'>
+        <div className='w-screen h-screen flex flex-col pt-[50px] bg-slate-200'>
+
+			<div className='h-[10%] flex items-center justify-center p-3 bg-slate-100'>
+				<form className='h-auto w-auto flex flex-row items-center justify-center gap-2 p-5'>
 					<input className='input' placeholder={t("search-template")}/>
 					<button>{icons.search}</button>
 				</form>
 			</div>
 
-			{/* Explore/Create */}
-			<div className='border-[1px] border-black h-[90%] flex flex-row'>
-				{/* Explore Templates */}
-				<div className='border-[1px] border-blue-400 h-full w-full md:w-[70%] flex flex-col items-center p-3'>
+			<div className='h-[90%] flex flex-row'>
+				<div className='h-full w-full md:w-[70%] flex flex-col items-center p-3'>
 					<p className='font-bold'>{t("explore-templates")}</p>
-					{/* MAP TEMPLATES */}
-					<div className='border-[1px] border-green-500 w-full h-full p-3'>
-
-					</div>
+					<ExploreTemplates />
 				</div>
-
-				{/* Create Template */}
 				<CreateTemplate userId={user.user.id}/>
 			</div>
         </div>
