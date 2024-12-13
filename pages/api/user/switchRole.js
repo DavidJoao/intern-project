c318e2185108c6b9a75c014953ac63277761bc7f
DavidJoao <davidsandoval596@gmail.com>
2024-12-12 22:10:22 -0800

    CREATED: Server Route that switches admin/removeAdmin

---
+++ pages/api/user/switchRole.js
@@ -0,0 +1,24 @@
+import { db } from "@/app/lib/prismaClient"
+
+export default async function POST (req, res) {
+    const { userId } = req.query;
+    try {
+        const foundUser = await db.user.findUnique({ where: { id: userId } })
+
+        if (!foundUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const updatedUser = await db.user.update({
+            where: { id: userId },
+            data: {
+                role: foundUser.role === "admin" ? "user" : "admin",
+            },
+        });
+
+        res.status(200).json({ user: updatedUser })
+    } catch (error) {
+        res.status(500).json({ error })
+        return error
+    }
+} 
