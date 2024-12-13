30a8d04536f31928325d99640a48d750fc90dddb
DavidJoao <davidsandoval596@gmail.com>
2024-12-12 22:06:23 -0800

    DELETED: Log of topics

--- pages/api/templates/topics.js
+++ pages/api/templates/topics.js
@@ -2,7 +2,6 @@ import { Topic } from "@prisma/client";
 
 export default async function GET(req, res){
     try {
-        console.log(Topic)
         res.status(200).json({ topics: Topic});
     } catch (error) {
         res.status(500).json({ error: error })
