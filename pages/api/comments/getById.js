ecc5949b15fa79b7e6e8a76bfe3835b8d3c07fee
DavidJoao <davidsandoval596@gmail.com>
2024-12-16 22:46:55 -0800

    CREATED: Server Route that gets comments by template Id

---
+++ pages/api/comments/getById.js
@@ -0,0 +1,31 @@
+import { db } from "@/app/lib/prismaClient"
+
+export default async function GET (req, res) {
+    const { templateId } = req.query;
+    try {
+        const foundComments = await db.comment.findMany({
+            where: { templateId },
+            include: {
+              user: {
+                select: {
+                  name: true
+                },
+              },
+            },
+          });
+      
+          const commentsWithUser = foundComments.map((comment) => ({
+            id: comment.id,
+            templateId: comment.templateId,
+            userId: comment.userId,
+            userName: comment.user.name,
+            content: comment.content,
+            createdAt: comment.createdAt,
+          }));
+      
+          res.status(200).json({ comments: commentsWithUser });
+    } catch (error) {
+        res.status(500).json({ error: error })
+        return error
+    }
+}
