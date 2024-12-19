530be770f58b50f0f1429447ebe87e8b40ddeea1
DavidJoao <davidsandoval596@gmail.com>
2024-12-18 23:06:43 -0800

    PASSED LoadQuestions to child component (Question)

--- src/app/components/questions/QuestionsSection.js
+++ src/app/components/questions/QuestionsSection.js
@@ -7,7 +7,7 @@ const ItemType = {
   QUESTION: "question",
 };
 
-const QuestionsSection = ({ questions, setQuestions }) => {
+const QuestionsSection = ({ questions, setQuestions, template, loadQuestions }) => {
   const moveQuestion = (dragIndex, hoverIndex) => {
     const updatedQuestions = [...questions];
     const [draggedItem] = updatedQuestions.splice(dragIndex, 1); 
@@ -20,12 +20,7 @@ const QuestionsSection = ({ questions, setQuestions }) => {
       <section className="flex flex-col w-full p-3 border-[1px] border-slate-200 gap-2">
         {questions?.length > 0 ? (
           questions.map((question, index) => (
-            <Question
-              key={question.id}
-              question={question}
-              index={index}
-              moveQuestion={moveQuestion}
-            />
+            <Question key={question.id} question={question} index={index} moveQuestion={moveQuestion} template={template} loadQuestions={loadQuestions} />
           ))
         ) : (
           <p className="text-center">No questions at the moment</p>
