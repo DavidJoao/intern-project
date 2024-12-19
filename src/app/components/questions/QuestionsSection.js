1c1420b8f8ad39d5d20476faa771f2ecde82a871
DavidJoao <davidsandoval596@gmail.com>
2024-12-19 13:33:23 -0800

    CREATE Function to update order of questions and update db

--- src/app/components/questions/QuestionsSection.js
+++ src/app/components/questions/QuestionsSection.js
@@ -1,23 +1,37 @@
-import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import React, { useRef } from "react";
+import React, { useCallback } from "react";
 import Question from "./Question";
-
-const ItemType = {
-  QUESTION: "question",
-};
+import { updateOrder } from "@/app/actions/questions";
 
 const QuestionsSection = ({ questions, setQuestions, template, loadQuestions }) => {
-  const moveQuestion = (dragIndex, hoverIndex) => {
+
+  const moveQuestion = useCallback((dragIndex, hoverIndex) => {
     const updatedQuestions = [...questions];
-    const [draggedItem] = updatedQuestions.splice(dragIndex, 1); 
+    const [draggedItem] = updatedQuestions.splice(dragIndex, 1);
     updatedQuestions.splice(hoverIndex, 0, draggedItem);
     setQuestions(updatedQuestions);
+
+    updateQuestionsOrder(updatedQuestions);
+  }, [questions, setQuestions]);
+
+  const updateQuestionsOrder = async (questions) => {
+    const orderedQuestions = questions.map((question, index) => ({
+      id: question.id,
+      order: index,
+    }));
+
+    try {
+      await updateOrder(template?.id, { questions: orderedQuestions })
+    } catch (error) {
+      console.log(error)
+      console.error("Error updating order:", error);
+    }
   };
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <section className="flex flex-col w-full p-3 border-[1px] border-slate-200 gap-2">
+      <section className="flex flex-col w-full p-4 border-[1px] border-slate-200 gap-4">
         {questions?.length > 0 ? (
           questions.map((question, index) => (
             <Question key={question.id} question={question} index={index} moveQuestion={moveQuestion} template={template} loadQuestions={loadQuestions} />
