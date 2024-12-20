75e547aa38f046d537c1a462706e91456330d874
DavidJoao <davidsandoval596@gmail.com>
2024-12-20 14:15:59 -0800

    ADD: Clear Create Question form on submit

--- src/app/components/questions/Question.js
+++ src/app/components/questions/Question.js
@@ -5,8 +5,7 @@ import { MdModeEditOutline } from "react-icons/md";
 import { FaTrash } from "react-icons/fa";
 import { deleteQuestionById, editQuestionById } from "@/app/actions/questions";
 
-const Question = ({ question, index, moveQuestion, template, loadQuestions }) => {
-
+const Question = ({ question, index, moveQuestion, template, loadQuestions, handleAnswerChange, formResetTrigger }) => {
 
 	const initialEdit = {
 		title: question?.title,
@@ -14,10 +13,12 @@ const Question = ({ question, index, moveQuestion, template, loadQuestions }) =>
 	}
 
 	const ref = useRef(null)
+	const inputRef = useRef(null);
 	const user = useAuth();
 
 	const [inputType, setInputType] = useState(<></>)
 	const [isEditing, setIsEditing] = useState(false)
+	const [isChecked, setIsChecked] = useState(false)
 	const [newEditContent, setNewEditContent] = useState(initialEdit)
 
     const ItemType = {
@@ -42,23 +43,42 @@ const Question = ({ question, index, moveQuestion, template, loadQuestions }) =>
 	drag(drop(ref))
 
 	useEffect(() => {
+		if (inputRef.current) {
+			if (question?.type === "checkbox") {
+				inputRef.current.checked = false;
+				setIsChecked(false);
+			} else {
+				inputRef.current.value = "";
+			}
+		}
+	}, [formResetTrigger]);
+
+	useEffect(() => {
 		switch (question.type) {
 			case 'text':
-				setInputType(<input className="input" type="text"/>)
+				setInputType(<input ref={inputRef} className="input" type="text" onChange={handleInputChange}/>)
 				break;
 			case 'multi_line':
-				setInputType(<textarea className="input resize-none" type="text"/>)
+				setInputType(<textarea ref={inputRef} className="input resize-none" type="text" onChange={handleInputChange}/>)
 				break;
 			case 'checkbox':
-				setInputType(<input className="input" type="checkbox"/>)
+				setInputType(<input ref={inputRef} className="input" type="checkbox" onChange={(e) => {
+					handleInputChange(e);
+					setIsChecked(e.target.checked)
+			}}/>)
 				break;
 			case 'integer':
-				setInputType(<input className="input" type="number"/>)
+				setInputType(<input ref={inputRef} className="input" type="number" onChange={handleInputChange}/>)
 				break;
 			default:
 				break;
 		}
 	}, [])
+
+	const handleInputChange = (event) => {
+		const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+		handleAnswerChange(question.id, value);
+	  };
 
 	const handleEdit = async (e) => {
 		e.preventDefault();
@@ -74,7 +94,7 @@ const Question = ({ question, index, moveQuestion, template, loadQuestions }) =>
 	}
 
 	return (
-		<form ref={ref} className="flex flex-col items-start w-full p-3 gap-2 border-[1px] rounded bg-gray-100 cursor-pointer" onSubmit={handleEdit}>
+		<div ref={ref} className="flex flex-col items-start w-full p-3 gap-2 border-[1px] rounded bg-gray-100 cursor-pointer" >
 			<div className="flex items-center justify-between w-full">
 				{ isEditing ? ( <input className="input" placeholder="Title" value={newEditContent.title} onChange={(e) => setNewEditContent({...newEditContent, ['title']: e.target.value})}/> ) 
 				: 
@@ -99,13 +119,13 @@ const Question = ({ question, index, moveQuestion, template, loadQuestions }) =>
 			( <p>{question.description}</p> ) }
 			{ isEditing && (
 				<div className="flex flex-row gap-3">
-					<button type="submit" className="blue-button w-auto">Edit</button>
+					<button type="submit" className="blue-button w-auto" onClick={handleEdit}>Edit</button>
 					<button className="blue-button w-auto" onClick={() => setIsEditing(!isEditing)}>Cancel</button>
 				</div>
 			)}
-			<label>Answer:</label>
-			{inputType}
-		</form>
+			<label className="text-xs text-slate-400">Answer:</label>
+			{inputType} {question?.type === 'checkbox' && <p>{isChecked ? "True" : "False"}</p>}
+		</div>
 	)
 }
 
