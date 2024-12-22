43517380b0713d1d9117289b73df647de328890a
DavidJoao <davidsandoval596@gmail.com>
2024-12-21 18:38:09 -0800

    CREATED: Server Route to edit template

---
+++ pages/api/templates/edit.js
@@ -0,0 +1,21 @@
+import { db } from "@/app/lib/prismaClient";
+
+export default async function PATCH(req, res) {
+    const { templateId } = req.query;
+    const form = req.body;
+    try {
+        const foundTemplate = await db.template.update({
+            where: { id: templateId },
+            data: { 
+                title: form.title,
+                description: form.description,
+                topic: form.topic
+            }
+        })
+        res.status(201).json({ updatedTemplate: foundTemplate })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error updating template', error })
+        return error;
+    }
+}
