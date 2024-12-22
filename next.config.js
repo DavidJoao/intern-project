20edd76a7aa8975f04ac5fc95ffa9f3e744b9d1a
DavidJoao <davidsandoval596@gmail.com>
2024-12-21 18:28:13 -0800

    ADDED: Source for Images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   i18n,
   images: {
-    domains: ["boothgrading-bucket.s3.us-west-1.amazonaws.com"]
+    domains: ["boothgrading-bucket.s3.us-west-1.amazonaws.com", "st4.depositphotos.com"]
   }
 }
