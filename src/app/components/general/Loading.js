a8d3b3f2b703f96600305c5d91db755e343b4502
DavidJoao <davidsandoval596@gmail.com>
2024-12-22 12:48:15 -0800

    CHANGED: Dark/Light theme of Loading component

--- src/app/components/general/Loading.js
+++ src/app/components/general/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='flex items-center justify-center w-screen h-screen text-center'>
+    <div className='flex items-center justify-center w-screen h-screen text-center general-bg'>
         <p className='text-center text-2xl font-semibold'>Loading, please wait...</p>
     </div>
   )
