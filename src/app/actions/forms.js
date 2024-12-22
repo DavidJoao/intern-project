b37a41b551c2656c7f60c16f2f8fc2b42a258ced
DavidJoao <davidsandoval596@gmail.com>
2024-12-22 12:46:59 -0800

    CREATE Actions: editAnswerById, fetchFormsByTemplateId, deleteFormById, and deleteAnswerById

--- src/app/actions/forms.js
+++ src/app/actions/forms.js
@@ -9,3 +9,43 @@ export const createForm = async (userId, templateId, answers) => {
         return error
     }
 }
+
+export const fetchFormsByTemplateId = async (templateId) => {
+    try {
+        const response = await axios.get(`/api/forms/getByTemplateId?templateId=${templateId}`)
+        return response;
+    } catch (error) {
+        console.log(error);
+        return error
+    }
+}
+
+export const editAnswerById = async (answerId, data) => {
+    try {
+        const response = await axios.patch(`/api/forms/editAnswer?answerId=${answerId}`, { data }, { headers: { "Content-Type": 'application/json'} })
+        return response;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+export const deleteAnswerById = async (answerId) => {
+    try {
+        const response = await axios.delete(`/api/forms/deleteAnswer?answerId=${answerId}`);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return error;  
+    }
+}
+
+export const deleteFormById = async (formId) => {
+    try {
+        const response = await axios.delete(`/api/forms/delete?formId=${formId}`)
+        return response
+    } catch (error) {
+        console.log(error);
+        return error
+    }
+}
