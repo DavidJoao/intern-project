9adcd9fc3d233fb012e13a07dccd5c5506deb80b
DavidJoao <davidsandoval596@gmail.com>
2024-12-21 18:35:34 -0800

    ADDED Order calculation when creating new templates

--- pages/api/templates/create.js
+++ pages/api/templates/create.js
@@ -3,13 +3,22 @@ import { db } from "@/app/lib/prismaClient"
 export default async function POST(req, res){
     try {
         const form = await req.body;
+
+        const maxOrderTemplate = await db.template.findFirst({
+            where: { creatorId: form.creatorId },
+            orderBy: { order: 'desc' },
+        });
+
+        const defaultOrder = maxOrderTemplate ? maxOrderTemplate.order + 1 : 1;
+
         const createdTemplate = await db.template.create({
             data: {
                 topic: form.topic,
                 title: form.name,
                 description: form.description,
                 imageUrl: form.imageUrl,
-                creatorId: form.creatorId
+                creatorId: form.creatorId,
+                order: form.order ?? defaultOrder,
             }
         })
         res.status(201).json({template: createdTemplate})
