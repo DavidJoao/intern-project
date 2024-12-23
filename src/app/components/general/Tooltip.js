53c987f5b38be63efd52db79e4a7d67df8d6fd21
DavidJoao <davidsandoval596@gmail.com>
2024-12-23 15:27:08 -0800

    CHANGE: Tooltip direction

--- src/app/components/general/Tooltip.js
+++ src/app/components/general/Tooltip.js
@@ -2,7 +2,7 @@ export default function Tooltip({ children, text }) {
     return (
       <div className="relative group">
         {children}
-        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2">
+        <span className="absolute top-1/2 left-full transform -translate-y-1/2 ml-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2 z-[50]">
           {text}
         </span>
       </div>
