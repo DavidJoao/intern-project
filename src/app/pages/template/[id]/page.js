7d44c806fc0994b74f8f42d49c77ccc22b836e86
DavidJoao <davidsandoval596@gmail.com>
2024-12-23 15:30:59 -0800

    CHANGE Styling and added dark theme:

--- src/app/pages/template/[id]/page.js
+++ src/app/pages/template/[id]/page.js
@@ -49,7 +49,7 @@ const Template = (context) => {
   return (
 		<>
 			{template ? (
-				<div className="w-screen h-auto md:h-screen flex flex-col pt-[50px] bg-slate-200 dark:bg-gray-800 dark:text-white">
+				<div className="w-screen min-h-screen h-auto flex flex-col pt-[50px] bg-slate-200 dark:bg-gray-800 dark:text-white">
 					<header className="w-full h-[20%] flex flex-row bg-center relative isolate p-3">
 						<div
 							className="absolute inset-0 bg-cover bg-center filter brightness-50 -z-10"
@@ -59,11 +59,11 @@ const Template = (context) => {
 							<p className="text-white text-sm">{template.description}</p>
 						</div>
 
-						<div className="w-[50%] flex flex-col items-end justify-evenly">
+						<div className="w-[50%] flex flex-col items-end justify-evenly gap-3">
 							<Like template={template}/>
 							<RoleBasedComponent condition={(user) => user.role === 'admin' || template.creatorId === user.id} user={user?.user}>
-								<Link href={`/pages/template/${template.id}/forms`} className="border blue-button w-[100px] gap-1 flex items-center justify-between text-sm"> Forms <IoArrowForwardCircleOutline />{" "} </Link>
-								<Link href={`/pages/template/${template.id}/settings`} className="border blue-button w-[100px] gap-1 flex items-center justify-between text-sm"> Settings <CiSettings />{" "} </Link>
+								<Link href={`/pages/template/${template.id}/forms`} className="new-theme-button w-[100px]"> Forms <IoArrowForwardCircleOutline />{" "} </Link>
+								<Link href={`/pages/template/${template.id}/settings`} className="new-theme-button w-[100px]"> Settings <CiSettings />{" "} </Link>
 							</RoleBasedComponent>
 						</div>
 					</header>
@@ -71,7 +71,7 @@ const Template = (context) => {
 					{/* MAIN CONTAINER */}
 					<main className="w-full h-full flex flex-col md:flex-row gap-2">
 						{/* Questions */}
-						<section className="flex flex-col w-full md:w-[70%] h-auto sm:h-[70%] md:h-full p-3 border-[1px] border-slate-200 gap-2">
+						<section className="flex flex-col w-full md:w-[70%] h-auto sm:h-[70%] md:h-full p-3 border-[1px] border-slate-200 dark:border-gray-700 rounded-lg gap-2">
 							<div className="flex flex-col items-center w-auto gap-4 border-3 rounded-lg p-4 lg:h-full lg:max-h-[600px] overflow-auto dark:bg-gray-800 bg-white border-gray-300 dark:border-gray-600">
 								<QuestionsSection questions={questions} setQuestions={setQuestions} template={template} loadQuestions={loadQuestions}/>
 								<RoleBasedComponent condition={(user) => user?.role === 'admin' || template.creatorId === user?.id} user={user?.user}>
