4336245896325e9b4c0d8fd20a9b9cd8e3b38be1
DavidJoao <davidsandoval596@gmail.com>
2024-12-23 12:04:19 -0800

    CREATE: Server Routes that fetch top 5 templates

---
+++ pages/api/templates/topTemplates.js
@@ -0,0 +1,28 @@
+import { db } from "@/app/lib/prismaClient";
+
+export default async function GET(req, res){
+    try {
+        const templates = await db.template.findMany({ 
+            include: {
+                _count: {
+                    select: {
+                        forms: true
+                    }
+                }
+            },
+            orderBy: {
+                forms:{
+                    _count: 'desc'
+                }
+            },
+            take: 5
+         })
+
+         res.status(200).json({ topTemplates: templates })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error fetching top 5 templates', error })
+        return error;
+    }
+}
