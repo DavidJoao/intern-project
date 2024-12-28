d76a0cc0451c30014d03f4d26538ac029f7fef8f
DavidJoao <davidsandoval596@gmail.com>
2024-12-27 17:23:43 -0800

    FIX

--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = {
   i18n,
   images: {
     domains: ["boothgrading-bucket.s3.us-west-1.amazonaws.com", "st4.depositphotos.com"]
-  }
+  },
+  env: {
+    DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  },
 }
+
