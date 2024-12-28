602b3557a2cdf419193d74d5d8f5c178a81d9008
DavidJoao <davidsandoval596@gmail.com>
2024-12-27 17:03:38 -0800

    CREATED: Server Route that retreives templates with datails, comments, etc that include query word

---
+++ pages/api/templates/search.js
@@ -0,0 +1,45 @@
+import { db } from "@/app/lib/prismaClient";
+
+export default async function GET (req, res) {
+    const { search } = req.query;
+    
+    try {
+        const templates = await db.template.findMany({
+            where: {
+              OR: [
+                { title: { contains: search, mode: 'insensitive' } },
+                { description: { contains: search, mode: 'insensitive' } },
+                {
+                  comments: {
+                    some: {
+                      content: { contains: search, mode: 'insensitive' },
+                    },
+                  },
+                },
+                {
+                  questions: {
+                    some: {
+                      OR: [
+                        { title: { contains: search, mode: 'insensitive' } },
+                        { description: { contains: search, mode: 'insensitive' } },
+                      ],
+                    },
+                  },
+                },
+              ],
+            },
+            include: {
+              comments: true,
+              questions: true,
+            },
+          });
+
+          res.status(200).json({ templates: templates })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: `Error fetching ${search} templates`, error })
+        return error;
+
+    }
+}
