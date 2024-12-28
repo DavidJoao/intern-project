39143a8f70fc43ca5f40d15b10079412717fee28
DavidJoao <davidsandoval596@gmail.com>
2024-12-27 17:04:00 -0800

    CREATED: Page that retreives templates with query

---
+++ src/app/pages/search/[query]/page.js
@@ -0,0 +1,41 @@
+'use client'
+import { searchTemplateAPI } from '@/app/actions/templates';
+import TemplateThumbnail from '@/app/components/templates/TemplateThumbnail';
+import React, { useEffect, useState } from 'react'
+
+const SearchQuery = (context) => {
+
+    const { query } = context.params;
+
+    const [templates, setTemplates] = useState(null)
+
+    useEffect(() => {
+        fetchSearch();
+    }, [])
+
+    const fetchSearch = async (e) => {
+        const { data } = await searchTemplateAPI(query);
+        setTemplates(data?.templates)
+    }
+
+    return (
+    <div className='pt-[50px] min-h-screen general-bg p-3 h-auto'>
+        <p>Showing Results for: "{query}"</p>
+        <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 p-3'>
+        { templates ? (
+            <>
+            { templates.map((template, index) => {
+                return (
+                    <TemplateThumbnail key={index} index={index} template={template} />
+                )
+            }) }
+            </>
+        ) : (
+            <p>No templates including {query} found</p>
+        ) }
+        </div>
+    </div>
+  )
+}
+
+export default SearchQuery
