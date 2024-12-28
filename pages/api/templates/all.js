a0eb01565078fc65a184c62117695985d086ece4
DavidJoao <davidsandoval596@gmail.com>
2024-12-28 14:18:16 -0800

    EDITED: Server Route, filtering depending on isPublic

--- pages/api/templates/all.js
+++ pages/api/templates/all.js
@@ -1,12 +1,37 @@
-import { db } from "@/app/lib/prismaClient"
+import { db } from "@/app/lib/prismaClient";
 
 export default async function GET(req, res) {
+    const { userId } = req.query
+    const user = await db.user.findUnique({ where: { id: userId } })
     try {
-        const templates = await db.template.findMany({})
-        res.status(200).json({ templates: templates })
-        return templates;
-    } catch (error){
-        res.status(500).json({error})
-        return error
+        if (user?.role === 'admin') {
+            const templates = await db.template.findMany({});
+            return res.status(200).json({ templates });
+        }
+
+        const templates = await db.template.findMany({
+            where: {
+                OR: [
+                    { isPublic: true },
+                    {
+                        isPublic: false,
+                        AND: [
+                            {
+                                OR: [
+                                    { allowedUsers: { hasSome: [user?.email] } },
+                                    { creatorId: userId }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        });
+        
+        return res.status(200).json({ templates });
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Error fetching templates", error });
     }
 }
