9e75599e75bb23faa350a26056f8fc3be94953f2
DavidJoao <davidsandoval596@gmail.com>
2024-12-29 15:10:54 -0800

    CHANGE Style of exploreTemplates and templateThumbail

--- src/app/components/templates/ExploreTemplates.js
+++ src/app/components/templates/ExploreTemplates.js
@@ -23,10 +23,10 @@ const ExploreTemplates = () => {
 	return (
         <div className="h-full min-h-full w-full md:w-[75%] flex flex-col items-center p-3">
             <p className='font-bold text-center text-lg dark:text-white'>{t("explore-templates")}</p>
-            <div className="border-2 border-slate-200 dark:border-gray-600 rounded shadow-lg w-full h-full md:h-[550px] p-4 bg-white dark:bg-gray-800">
+            <div className="border-2 border-slate-200 dark:border-gray-600 rounded shadow-lg w-full min-h-full md:h-auto p-4 bg-white dark:bg-gray-800">
                 <div className='w-full'>
                     <p className='font-bold mb-3 dark:text-white'>{t("top-five")}</p>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
                         {topTemplates ? (
                             topTemplates.map((template, index) => {
                                 return (
@@ -42,7 +42,7 @@ const ExploreTemplates = () => {
                     </div>
                 </div>
 
-                <div className="w-full mt-6">
+                <div className="w-full mt-6 h-auto p-2">
                     <p className="font-bold mb-3 dark:text-white">{t("all-templates")}</p>
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4">
                         {templates ? (
--- src/app/components/templates/TemplateThumbnail.js
+++ src/app/components/templates/TemplateThumbnail.js
@@ -8,7 +8,7 @@ import { useAuth } from '@/app/hooks/useAuth';
 const TemplateThumbnail = ({ template, index }) => {
 
   const user = useAuth();
-
+  
   return (
     <Tooltip text={template.description}>
         <div className='rounded p-3 flex flex-col items-center justify-center cursor-pointer dark:bg-gray-900 dark:hover:bg-gray-800 bg-slate-200 hover:bg-slate-100 transition-all shadow-md w-full h-full' onClick={() => {
@@ -26,6 +26,7 @@ const TemplateThumbnail = ({ template, index }) => {
         <Image className="rounded-md object-cover" alt='template' src={template.imageUrl} width={100} height={100} />
         </div>
             <p className="text-xs font-bold text-center dark:text-white text-black">{template.title}</p>
+            <p className="text-xs font-bold text-center dark:text-white text-black"> by {template?.creator?.name}</p>
             <ReactTimeago className="text-xs text-gray-500 dark:text-gray-300 text-center" date={template.createdAt}/>
             {!template.isPublic && 
               user?.user?.role !== 'admin' && 
