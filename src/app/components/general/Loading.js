d9a644aff981a3d44285a3458d5cf0fbaf1f977d
DavidJoao <davidsandoval596@gmail.com>
2024-12-29 19:10:49 -0800

    ADDED: Loader and translation

--- src/app/components/general/Loading.js
+++ src/app/components/general/Loading.js
@@ -1,9 +1,13 @@
+import { icons } from '@/app/lib/icons'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 const Loading = () => {
+  const { t } = useTranslation("common")
   return (
-    <div className='flex items-center justify-center w-screen h-screen text-center general-bg'>
-        <p className='text-center text-2xl font-semibold'>Loading, please wait...</p>
+    <div className='flex flex-col items-center justify-center w-screen h-screen text-center general-bg gap-3'>
+        <p className='text-center text-2xl font-semibold'>{t("loading")}</p>
+        <p className='text-[100px] dark:text-white'>{icons.loading}</p>
     </div>
   )
 }
