9d08722af78229dba2b2486452f4efbdfdad3673
DavidJoao <davidsandoval596@gmail.com>
2024-12-29 19:08:38 -0800

    CHANGED: Email input to lowercase

--- src/app/pages/signup/page.js
+++ src/app/pages/signup/page.js
@@ -7,6 +7,7 @@ import { navigate } from "@/app/lib/redirect"
 import { logSession } from "@/app/actions/session"
 import { useRouter } from "next/navigation"
 import { useTranslation } from "react-i18next"
+import { icons } from "@/app/lib/icons"
 
 const Signup = () => {
 
@@ -16,6 +17,7 @@ const Signup = () => {
     const [errorMessage, setErrorMessage] = useState("")
     const [successMessage, setSuccessMessage] = useState("")
     const [buttonStatus, setButtonStatus] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
     const { register, setError, clearErrors, handleSubmit, watch, formState: { errors } } = useForm();
     const password = watch("password");
@@ -24,6 +26,7 @@ const Signup = () => {
 
 
     const onSubmit = async (data) => {
+        setIsLoading(true)
         setErrorMessage("")
         const response = await registerUser(data)
         if (response.status === 201){
@@ -32,8 +35,10 @@ const Signup = () => {
                 navigate('/pages/login')
                 setSuccessMessage("")
             }, 2000)
+            setIsLoading(false)
         } else if (response.status === 500){
             setErrorMessage("Email Already In Use")
+            setIsLoading(false)
         }
     }
 
@@ -74,14 +79,14 @@ const Signup = () => {
 				<h1 className="font-bold text-3xl">{t("signup")}</h1>
 				<form className="border-[1px] border-primary dark:border-none rounded w-full md:w-[400px] lg:w-[500px] p-3 white-theme center-col gap-3" onSubmit={handleSubmit(onSubmit)}>
 					<label>Email</label>
-					<input required className="input w-[70%]" {...register("email", { required: true })}/>
+					<input required className="input w-[70%] lowercase" {...register("email", { required: true })}/>
 					<label>{t("name")}</label>
 					<input required className="input w-[70%]" {...register("name", { required: true })}/>
 					<label>{t("password")}</label>
 					<input required name="password" className="input w-[70%]" type="password" {...register("password", { required: true })}/>
 					<label>{t("confirm-password")}</label>
 					<input required name="confirmPassword" className="input w-[70%]" type="password" {...register("confirmPassword", { required: true })}/>
-					<button type="submit" className="theme-button w-[70%]" disabled={buttonStatus}> {t("signup")} </button>
+					<button type="submit" className="theme-button w-[70%] flex justify-center" disabled={buttonStatus}> { isLoading ? icons.loading : t("signup")} </button>
                     <p className="error-message">{errorMessage}</p>
                     <p className="success-message text-center">{successMessage}</p>
 				</form>
