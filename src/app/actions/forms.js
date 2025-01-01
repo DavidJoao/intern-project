7f66c3e0fa11fb46c64e1c33b8d9322a4e499036
DavidJoao <davidsandoval596@gmail.com>
2025-01-01 13:37:03 -0800

    CREATED: Actions to fetch Forms by user id and template id

--- src/app/actions/forms.js
+++ src/app/actions/forms.js
@@ -20,6 +20,16 @@ export const fetchFormsByTemplateId = async (templateId) => {
     }
 }
 
+export const fetchFormsByUserAndTemplate = async (templateId, userId) => {
+    try {
+        const response = await axios.get(`/api/forms/getByUserAndTemplateId?templateId=${templateId}&userId=${userId}`)
+        return response;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
 export const editAnswerById = async (answerId, data) => {
     try {
         const response = await axios.patch(`/api/forms/editAnswer?answerId=${answerId}`, { data }, { headers: { "Content-Type": 'application/json'} })
