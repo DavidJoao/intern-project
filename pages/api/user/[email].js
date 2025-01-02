850b4d687fc779e7b60ae79fdcae70748420a018
DavidJoao <davidsandoval596@gmail.com>
2025-01-02 14:47:03 -0800

    ADDED: Check for user nonexistance, or user status

--- pages/api/user/[email].js
+++ pages/api/user/[email].js
@@ -3,14 +3,17 @@ import { db } from "@/app/lib/prismaClient";
 export default async function GET(req, res) {
     try {
         const { email } = req.query;
-        console.log(email)
         const foundUser = await db.user.findUnique({ where: { email: email } });
-        if (!foundUser){
-            res.status(404).json({ error: 'User not found' });
+
+        if (!foundUser) {
+            return res.status(404).json({ error: 'Account does not exist' });
         }
 
-        res.status(200).json({ user: foundUser })
-        return foundUser;
+        if (foundUser.status === 'blocked') {
+            return res.status(400).json({ error: 'Account blocked' });
+        }
+
+        return res.status(200).json({ user: foundUser });
     } catch (error) {
         res.status(500).json({ error: error })
         console.log(error)
