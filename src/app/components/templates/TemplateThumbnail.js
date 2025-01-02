44467f6170b72c898d555a1d9ef265edfc45fc04
DavidJoao <davidsandoval596@gmail.com>
2025-01-02 14:49:49 -0800

    CHANGE: Converted Thumbnail to button

--- src/app/components/templates/TemplateThumbnail.js
+++ src/app/components/templates/TemplateThumbnail.js
@@ -10,18 +10,19 @@ const TemplateThumbnail = ({ template, index }) => {
   const user = useAuth();
   
   return (
+    <button className='rounded p-3 flex flex-col items-center justify-center cursor-pointer dark:bg-gray-900 dark:hover:bg-gray-800 bg-slate-200 hover:bg-slate-100 transition-all shadow-md w-full h-full' onClick={() => {
+      if (template.isPublic) {
+        navigate(`/pages/template/${template.id}`);
+      } else if (
+        template.allowedUsers.includes(user?.user?.email) ||
+        template?.creatorId === user?.user?.id ||
+        user?.user?.role === 'admin'
+      ) {
+        navigate(`/pages/template/${template.id}`);
+      }
+    }}>
     <Tooltip text={template.description}>
-        <div className='rounded p-3 flex flex-col items-center justify-center cursor-pointer dark:bg-gray-900 dark:hover:bg-gray-800 bg-slate-200 hover:bg-slate-100 transition-all shadow-md w-full h-full' onClick={() => {
-          if (template.isPublic) {
-            navigate(`/pages/template/${template.id}`);
-          } else if (
-            template.allowedUsers.includes(user?.user?.email) ||
-            template?.creatorId === user?.user?.id ||
-            user?.user?.role === 'admin'
-          ) {
-            navigate(`/pages/template/${template.id}`);
-          }
-        }}>
+        <div>
         <div className='flex justify-center items-center mb-2'>
         <Image className="rounded-md object-cover" alt='template' src={template.imageUrl} width={100} height={100} />
         </div>
@@ -36,6 +37,7 @@ const TemplateThumbnail = ({ template, index }) => {
             )}
         </div>
     </Tooltip>
+    </button>
   )
 }
 
