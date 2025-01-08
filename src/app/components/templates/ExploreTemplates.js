3a38f92b20b63a0ed8a67e6ae4d28681cff348f2
DavidJoao <davidsandoval596@gmail.com>
2025-01-07 19:24:47 -0800

    ADDED: Dynamic import to TemplateThumbnail

--- src/app/components/templates/ExploreTemplates.js
+++ src/app/components/templates/ExploreTemplates.js
@@ -1,13 +1,14 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/provider'
-import TemplateThumbnail from './TemplateThumbnail';
 import { fetchTopTemplates } from '@/app/actions/templates';
 import { useTranslation } from 'react-i18next';
-import Loading from '../general/Loading';
-
+import dynamic from 'next/dynamic';
 
 const ExploreTemplates = () => {
+
+    const TemplateThumbnail = dynamic(() => import("@/app/components/templates/TemplateThumbnail"))
+
 	const { templates, loadAllTemplates } = useAppContext()
     const { t } = useTranslation('common');
 	const [topTemplates, setTopTemplates] = useState(null)
@@ -28,7 +29,6 @@ const ExploreTemplates = () => {
 	return (
         <div className="h-full min-h-full w-full md:w-[75%] flex flex-col items-center p-3">
             <p className='font-bold text-center text-lg dark:text-white'>{t("explore-templates")}</p>
-            { templates && topTemplates ? (
             <div className="border-2 border-slate-200 dark:border-gray-600 rounded shadow-lg w-full min-h-full md:h-auto p-4 bg-white dark:bg-gray-800">
                 <div className='w-full'>
                     <p className='font-bold mb-3 dark:text-white'>{t("top-five")}</p>
@@ -36,7 +36,7 @@ const ExploreTemplates = () => {
                         {topTemplates ? (
                             topTemplates.map((template, index) => {
                                 return (
-                                    <div key={index} className='rounded dark:bg-gray-700 p-2 flex flex-col items-center justify-center gap-1'>
+                                    <div key={index} className='rounded bg-gray-100 dark:bg-gray-700 p-2 flex flex-col items-center justify-center gap-1'>
                                         <TemplateThumbnail key={index} template={template} />
                                         <p className='text-center dark:text-white'>{template?._count?.forms} {t("forms-filled")}</p>
                                     </div>
@@ -54,7 +54,7 @@ const ExploreTemplates = () => {
                         {templates ? (
                             templates.map((template, index) => {
                                 return (
-                                    <div key={index} className='rounded dark:bg-gray-700 p-2 flex flex-col items-center justify-center gap-1'>
+                                    <div key={index} className='rounded bg-gray-100 dark:bg-gray-700 p-2 flex flex-col items-center justify-center gap-1'>
                                         <TemplateThumbnail key={index} template={template} />
                                     </div>
                                 )
@@ -65,9 +65,6 @@ const ExploreTemplates = () => {
                     </div>
                 </div>
             </div>
-            ) : (
-                <Loading />
-            ) }
         </div>
 	)
 }
