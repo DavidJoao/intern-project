1e51009762142d11d94dcd6c9257321f0f0918f0
DavidJoao <davidsandoval596@gmail.com>
2025-01-07 19:23:20 -0800

    CHANGE: Style of mobile navbar

--- src/app/components/general/MobileNavMenu.js
+++ src/app/components/general/MobileNavMenu.js
@@ -47,64 +47,127 @@ const MobileNavMenu = ({ setIsOpen, isOpen, session}) => {
     }
 
     return (
-        <>
-        { session ? (
-            <div className={`pt-[50px] p-3 ${ isOpen === true ? 'fixed' : 'hidden' } md:hidden flex-col gap-2 items-center justify-center bg-white dark:bg-gray-800 z-[200] top-10 w-full`}>
-                <Link className="nav-button border-b w-full p-2" href={"/pages/profile"} onClick={() => setIsOpen(false)}> <CiUser /> {t("profile")} </Link>
-                <Link className="nav-button border-b w-full p-2" href={"/pages/home"} onClick={() => setIsOpen(false)}> <MdOutlineDashboard /> {t("dashboard")}</Link>
-                <Link className="nav-button border-b w-full p-2" href={"/pages/settings"} onClick={() => setIsOpen(false)}> <CiSettings /> {t("settings")} </Link>
-                <div className="nav-button border-b w-full p-2"> <MdLanguage />
-                    <select
-                        className="bg-transparent text-gray-700 dark:text-gray-300 rounded border border-gray-300 dark:border-gray-600 p-1 focus:outline-none focus:ring focus:ring-blue-300 dark:focus:ring-blue-700"
-                        onChange={e => changeLanguage(e.target.value)}>
-                        <option value={"en"}>EN</option>
-                        <option value={"es"}>ES</option>
-                    </select>
-                </div>
-                {theme === "dark" ? (
-                    <button className="nav-button border-b w-full p-4" onClick={() => toggleTheme()}>
-                        <CiLight />
-                    </button>
-                ) : (
-                    <button className="nav-button border-b w-full p-4" onClick={() => toggleTheme()}>
-                        <GoMoon />
-                    </button>
-                )}
-                <button className="nav-button border-b w-full p-2" onClick={() => {
-                    logoutUser()
-                    setIsOpen(false)
-                    }}> <CiLogout />{t("logout")}</button>
-                <form className="h-auto w-auto flex flex-row items-center justify-center gap-2 p-5 mx-auto">
-                    <input className="input w-full dark:bg-gray-700 dark:text-white dark:border-gray-600" placeholder={t("search-template")} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
-                    <button className="dark:text-gray-400" onClick={() => setIsOpen(false)}>{icons.search}</button>
-                    {Array.isArray(matchingTags) && matchingTags.length > 0 && searchQuery !== '' && (
-                            <ul className="absolute top-full w-[200px] bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-lg shadow-lg z-50 p-1 flex flex-col gap-1">
-                                <li className='new-theme-gray-button cursor-pointer' onClick={(e) => handleSearch(e, searchQuery)}>{searchQuery}...</li>
-                                {matchingTags.map((tag, index) => (
-                                    <li key={index} onClick={async (e) => {
-                                        setSearchQuery(tag)
-                                        await handleSearch(e, tag)
-
-                                    }} className='new-theme-gray-button cursor-pointer'> {tag} </li>
-                                ))}
-                            </ul>
+		<>
+			{session ? (
+				<div
+					className={`pt-[50px] p-3 ${
+						isOpen === true ? "fixed" : "hidden"
+					} md:hidden flex-col gap-2 items-center justify-center bg-white dark:bg-gray-800 z-[200] top-10 w-full`}>
+					<Link
+						className="nav-button border-b w-full p-2"
+						href={"/pages/profile"}
+						onClick={() => setIsOpen(false)}>
+						{" "}
+						<CiUser /> {t("profile")}{" "}
+					</Link>
+					<Link
+						className="nav-button border-b w-full p-2"
+						href={"/pages/home"}
+						onClick={() => setIsOpen(false)}>
+						{" "}
+						<MdOutlineDashboard /> {t("dashboard")}
+					</Link>
+					<Link
+						className="nav-button border-b w-full p-2"
+						href={"/pages/settings"}
+						onClick={() => setIsOpen(false)}>
+						{" "}
+						<CiSettings /> {t("settings")}{" "}
+					</Link>
+					<div className="nav-button border-b w-full p-2">
+						{" "}
+						<MdLanguage />
+						<select
+							className="bg-transparent text-gray-700 dark:text-gray-300 rounded border border-gray-300 dark:border-gray-600 p-1 focus:outline-none focus:ring focus:ring-blue-300 dark:focus:ring-blue-700"
+							onChange={e => changeLanguage(e.target.value)}>
+							<option value={"en"}>EN</option>
+							<option value={"es"}>ES</option>
+						</select>
+					</div>
+					{theme === "dark" ? (
+						<button
+							className="nav-button border-b w-full p-4"
+							onClick={() => toggleTheme()}>
+							<CiLight />
+						</button>
+					) : (
+						<button
+							className="nav-button border-b w-full p-4"
+							onClick={() => toggleTheme()}>
+							<GoMoon />
+						</button>
+					)}
+					<button
+						className="nav-button border-b w-full p-2"
+						onClick={() => {
+							logoutUser()
+							setIsOpen(false)
+						}}>
+						{" "}
+						<CiLogout />
+						{t("logout")}
+					</button>
+					<form className="h-auto w-auto flex flex-row items-center justify-center gap-2 p-5 mx-auto">
+						<input
+							className="input w-full dark:bg-gray-700 dark:text-white dark:border-gray-600"
+							placeholder={t("search-template")}
+							value={searchQuery}
+							onChange={e => setSearchQuery(e.target.value)}
+						/>
+						<button className="dark:text-gray-400" onClick={() => setIsOpen(false)}>
+							{icons.search}
+						</button>
+						{Array.isArray(matchingTags) &&
+							matchingTags.length > 0 &&
+							searchQuery !== "" && (
+								<ul className="absolute top-full w-[200px] bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-lg shadow-lg z-50 p-1 flex flex-col gap-1">
+									<li
+										className="new-theme-gray-button cursor-pointer"
+										onClick={e => handleSearch(e, searchQuery)}>
+										{searchQuery}...
+									</li>
+									{matchingTags.map((tag, index) => (
+										<li
+											key={index}
+											onClick={async e => {
+												setSearchQuery(tag)
+												await handleSearch(e, tag)
+											}}
+											className="new-theme-gray-button cursor-pointer">
+											{" "}
+											{tag}{" "}
+										</li>
+									))}
+								</ul>
+							)}
+					</form>
+				</div>
+			) : (
+				<div className={`pt-[50px] p-3 ${ isOpen === true ? "flex" : "hidden" } md:hidden flex-col gap-2 items-center justify-center bg-white dark:bg-gray-800 dark:text-white`}>
+					<div className="nav-button border-b w-full p-2">
+						{" "}
+						<MdLanguage />
+						<select
+							className="bg-transparent text-gray-700 dark:text-gray-300 rounded border border-gray-300 dark:border-gray-600 p-1 focus:outline-none focus:ring focus:ring-blue-300 dark:focus:ring-blue-700"
+							onChange={e => changeLanguage(e.target.value)}>
+							<option value={"en"}>EN</option>
+							<option value={"es"}>ES</option>
+						</select>
+					</div>
+                    <div className='nav-button border-b w-full p-2'>
+                        {theme === "dark" ? (
+                            <button onClick={() => toggleTheme()}>
+                                <CiLight className="mx-auto" />
+                            </button>
+                        ) : (
+                            <button onClick={() => toggleTheme()}>
+                                <GoMoon className="mx-auto" />
+                            </button>
                         )}
-                </form>
-            </div>
-        ) : (
-            <div className={`pt-[50px] p-3 ${ isOpen === true ? 'flex' : 'hidden' } md:hidden flex-col gap-2 items-center justify-center bg-white dark:bg-gray-800`}>
-                <div className="nav-button border-b w-full p-2"> <MdLanguage />
-                    <select
-                        className="bg-transparent text-gray-700 dark:text-gray-300 rounded border border-gray-300 dark:border-gray-600 p-1 focus:outline-none focus:ring focus:ring-blue-300 dark:focus:ring-blue-700"
-                        onChange={e => changeLanguage(e.target.value)}>
-                        <option value={"en"}>EN</option>
-                        <option value={"es"}>ES</option>
-                    </select>
-                </div>
-            </div>
-            
-        ) }
-        </>
+                    </div>
+				</div>
+			)}
+		</>
 	)
 }
 
