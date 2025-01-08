ebb1c0699d63462d49ccd7c27daa3a6966d27e8a
DavidJoao <davidsandoval596@gmail.com>
2025-01-07 19:27:39 -0800

    FIX: Active/blocked label conditional render

--- src/app/pages/settings/page.js
+++ src/app/pages/settings/page.js
@@ -92,7 +92,7 @@ const Settings = () => {
                     <td className="p-4">
                         <span
                         className={`px-2 py-1 text-xs font-semibold rounded ${
-                            user.status === "Active"
+                            user.status === "active"
                             ? "bg-green-100 text-green-700"
                             : "bg-red-100 text-red-700"
                         }`}
